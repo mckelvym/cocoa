57e009e23741e203d67b9d2ba19047fdc1a87732
Mark McKelvy <65346059+mckelvym@users.noreply.github.com>
2010-05-14 21:00:45 +0000

    Chapter 19..

---
+++ TypingTutor/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TypingTutor
+//
+//  Created by mark mckelvy on 5/14/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
